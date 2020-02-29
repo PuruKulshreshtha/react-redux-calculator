ed58d47899d025445141f538432df61a36f66ded
manish-dalal <manish.dalal@daffodilsw.com>
2020-02-29 17:34:09 +0530

    Updates

---
+++ precache-manifest.e3ba49eaa863faa52bf1d9ccec007ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7204283f7d853b4ff0677cdc3358fe",
+    "url": "/react-redux-calculator/index.html"
+  },
+  {
+    "revision": "3aec8a20c49b2590ef22",
+    "url": "/react-redux-calculator/static/css/main.474790b8.chunk.css"
+  },
+  {
+    "revision": "8b995421a80a51fa0cf4",
+    "url": "/react-redux-calculator/static/js/2.870c56ab.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-redux-calculator/static/js/2.870c56ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aec8a20c49b2590ef22",
+    "url": "/react-redux-calculator/static/js/main.de176330.chunk.js"
+  },
+  {
+    "revision": "fce921b6a6b37a1200d8",
+    "url": "/react-redux-calculator/static/js/runtime-main.e695f157.js"
+  }
+]);
